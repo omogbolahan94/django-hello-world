08f6ebcf9fe18f394cc957837bc980c0774f3f01
omogbolahan94 <gabrielomogbolahan1@gmail.com>
2025-05-13 07:00:42 +0100

    added template view to Page app

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+# here is a function-based view
 def homePageView(request):
     return HttpResponse('Hello, World!')
+
+
+# here is a class-based view
+class HomePageView(TemplateView):
+    """
+    TemplateView already contains all the logic needed to display
+    our template, we just need to specify the template’s name
+    """
+    template_name = 'home.html'
